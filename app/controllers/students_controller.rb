030dad28a6d55fc1eb92aef37cb7b61b42ed0381
dunow <dunow33@hotmail.com>
2021-01-30 02:49:42 -0500

    commit tree 57d933544422a9f4855ae72bbd33cf0423f368f5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,20 +5,14 @@ class StudentsController < ApplicationController
   end
 
   def show
-    set_student
+    @student = Student.find(params[:id])
   end
 
   def new
   end
 
-  def create 
+  def create
     Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
     redirect_to students_path
-  end 
-
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  end
 end
